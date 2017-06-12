cae3bc2e9f0986ed86c36f09d050d55267e5f4ae
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2017-06-12 08:09:53 +0200

    WIP: reduce log output

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+set -e # Abort on error.
+
+export PING_SLEEP=30s
+export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export BUILD_OUTPUT=$WORKDIR/build.out
+
+touch $BUILD_OUTPUT
+
+dump_output() {
+   echo Tailing the last 500 lines of output:
+   tail -500 $BUILD_OUTPUT
+}
+error_handler() {
+  echo ERROR: An error was encountered with the build.
+  dump_output
+  exit 1
+}
+
+# If an error occurs, run our error handler to output a tail of the build.
+trap 'error_handler' ERR
+
+# Set up a repeating loop to send some output to Travis.
+bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+PING_LOOP_PID=$!
+
 if [[ $(uname) == Darwin ]]; then
   export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
 elif [[ $(uname) == Linux ]]; then
@@ -22,5 +47,16 @@ cmake $src_dir \
          -DENABLE_PYTHON=1 \
          -DENABLE_FORTRAN=0
 
-make
-make install
+make -j $CPU_COUNT >> $BUILD_OUTPUT 2>&1
+export ECCODES_TEST_VERBOSE_OUTPUT=1
+eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib
+if [[ $(uname) == Linux ]]; then
+ctest -j $CPU_COUNT >> $BUILD_OUTPUT 2>&1
+fi
+make install >> $BUILD_OUTPUT 2>&1
+
+# The build finished without returning an error so dump a tail of the output.
+dump_output
+
+# Nicely terminate the ping output loop.
+kill $PING_LOOP_PID
