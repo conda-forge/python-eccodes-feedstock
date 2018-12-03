6bec20c1e5dcc389f65f7812026ef127c914eab8
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2018-12-03 18:06:45 +0100

    FIX: find `libm` workaround as used in `eccodes-feedstock`

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,10 @@ export PING_SLEEP=30s
 export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export BUILD_OUTPUT=$WORKDIR/build.out
 
+if [[ "$c_compiler" == "gcc" ]]; then
+  export PATH="${PATH}:${BUILD_PREFIX}/${HOST}/sysroot/usr/lib"
+fi
+
 touch $BUILD_OUTPUT
 
 dump_output() {
