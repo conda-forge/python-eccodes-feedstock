6675d3f38f9dbdb864d43751497dbbcab8c0ddb2
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-08-27 06:52:25 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,42 +2,4 @@
 
 set -e
 
-if [[ "$c_compiler" == "gcc" ]]; then
-  export PATH="${PATH}:${BUILD_PREFIX}/${HOST}/sysroot/usr/lib"
-fi
-
-if [[ $(uname) == Darwin ]]; then
-  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-elif [[ $(uname) == Linux ]]; then
-  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
-fi
-
-export PYTHON="$PYTHON"
-export PYTHON_LDFLAGS="$PREFIX/lib"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
-export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
-
-mkdir ../build && cd ../build
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      -D ENABLE_JPG=1 \
-      -D ENABLE_NETCDF=1 \
-      -D ENABLE_PNG=1 \
-      -D ENABLE_PYTHON=1 \
-      -D ENABLE_FORTRAN=0 \
-      -D ENABLE_AEC=1 \
-      $SRC_DIR
-
-make -j $CPU_COUNT
-
-export ECCODES_TEST_VERBOSE_OUTPUT=1
-eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib
-
-if [[ $(uname) == Linux ]]; then
-  ctest -j $CPU_COUNT
-fi
-
-make install
-
-if [[ "${PY3K}" == "1" ]]; then
-  ${PYTHON} -m pip install eccodes-python -vv --no-deps
-fi
+${PYTHON} -m pip install . -vv --no-deps
