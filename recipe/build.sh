7577c718f57fd196715288c4432ac32ae9e5ef27
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-08-27 08:03:28 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -e
 
 # we need to add the compiled extension to the package
 # so we mangle the MANIFEST.in
-echo "recursive-include gribapi *.${SHLIB_EXT}" >> MANIFEST.in
+echo "recursive-include gribapi *${SHLIB_EXT}" >> MANIFEST.in
 
 ${PYTHON} builder.py
 ${PYTHON} -m pip install . -vv --no-deps
